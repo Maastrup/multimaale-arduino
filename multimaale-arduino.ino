5c2ff788f8e4064a4fc2deb715a87f1b80371c07
Maastrup <karljohan@maastrup.dk>
2018-10-29 13:19:54 +0100

    Setup

---
+++ multimaale-arduino.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, e = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+const int buttonPin = 2;
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+
+int biggest = 734;
+int smallest = 37;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ting = analogRead(A0);
+//  if (ting < smallest) {
+//    smallest = ting;
+//    Serial.println(smallest);
+//  }
+  long temp = map(ting, smallest, biggest, 0, 100);
+  // float finalTemp = temp / 100.0f;
+  Serial.println(temp);
+}
